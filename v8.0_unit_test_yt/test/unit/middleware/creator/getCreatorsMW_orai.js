545d3d23015961afabb191421e1d0ac2fdcf28db
Pap Dániel <papdaniel9977@gmail.com>
2023-11-30 14:13:32 +0100

    kis kod orarol

---
+++ v8.0_unit_test_yt/test/unit/middleware/creator/getCreatorsMW_orai.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+var getCreatorByIdMW = require('../../../../middleware/creator/getCreatorsMW');
+
+describe("get all creators", function(){
+    it("betölti összes cratorst a db-ből", function(done){
+
+    })
+})
+
+//istanbul - nyc (nem kell)
+
+//if nél 2 teszt, .then, .catch nél is legyen 2
